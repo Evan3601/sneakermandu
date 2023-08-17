fe8e5a94742bdc54b17af7f30f51f77418a0bc6c
Sandeep-Github01 <sandeepmhrjzn@gmail.com>
2023-08-17 23:33:43 +0545

    quick_view.php

--- quick_view.php
+++ quick_view.php
@@ -20,7 +20,7 @@ include 'components/wishlist_cart.php';
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>quick view</title>
+   <title>Quick view</title>
    
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -52,18 +52,18 @@ include 'components/wishlist_cart.php';
       <div class="row">
          <div class="image-container">
             <div class="main-image">
-               <img src="uploaded_img/<?= $fetch_product['image_01']; ?>" alt="">
+               <img src="project_images/<?= $fetch_product['image_01']; ?>" alt="">
             </div>
             <div class="sub-image">
-               <img src="uploaded_img/<?= $fetch_product['image_01']; ?>" alt="">
-               <img src="uploaded_img/<?= $fetch_product['image_02']; ?>" alt="">
-               <img src="uploaded_img/<?= $fetch_product['image_03']; ?>" alt="">
+               <img src="project_images/<?= $fetch_product['image_01']; ?>" alt="">
+               <img src="project_images/<?= $fetch_product['image_02']; ?>" alt="">
+               <img src="project_images/<?= $fetch_product['image_03']; ?>" alt="">
             </div>
          </div>
          <div class="content">
             <div class="name"><?= $fetch_product['name']; ?></div>
             <div class="flex">
-               <div class="price"><span>$</span><?= $fetch_product['price']; ?><span>/-</span></div>
+               <div class="price"><span>Rs.</span><?= $fetch_product['price']; ?><span>/-</span></div>
                <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
             </div>
             <div class="details"><?= $fetch_product['details']; ?></div>
