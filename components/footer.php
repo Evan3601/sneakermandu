e4d2da6f5c92eb32a444bcd369b746958080e386
Sandeep-Github01 <sandeepmhrjzn@gmail.com>
2023-08-17 23:36:05 +0545

    components

--- components/footer.php
+++ components/footer.php
@@ -3,23 +3,23 @@
    <section class="grid">
 
       <div class="box">
-         <h3>quick links</h3>
-         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
-         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
-         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
-         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+         <h3>Quick Links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
       </div>
 
       <div class="box">
-         <h3>extra links</h3>
-         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
-         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
-         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
-         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+         <h3>Extra Links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
       </div>
 
       <div class="box">
-         <h3>contact us</h3>
+         <h3>Contact us</h3>
          <a href="tel:9808123456"><i class="fas fa-phone"></i> +977 9808123456</a>
          <a href="tel:01-5544321"><i class="fas fa-phone"></i> +977 15544321</a>
          <a href="mailto:sneakermandu@gmail.com"><i class="fas fa-envelope"></i> sneakersahead@gmail.com</a>
@@ -27,7 +27,7 @@
       </div>
 
       <div class="box">
-         <h3>follow us</h3>
+         <h3>Follow us</h3>
          <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
          <a href="#"><i class="fab fa-instagram"></i>instagram</a>
          
